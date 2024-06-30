9d1d71110ba439af73fdfa747a5f1e106dc5e71e
Ylli Neziri <ylli.neziri@swisscom.com>
2024-06-30 02:24:53 +0200

    feat: added frontend

---
+++ frontend/FAInalystApp.swift
@@ -0,0 +1,17 @@
+//
+//  FAInalystApp.swift
+//  FAInalyst
+//
+//  Created by taaneyl1 on 29.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct FAInalystApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
